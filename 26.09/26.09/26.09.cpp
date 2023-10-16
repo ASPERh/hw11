d4f600580d6bba3e451acaeec8a39e9d0b6fcd1a
ASPER <144143767+ASPERh@users.noreply.github.com>
2023-10-16 13:10:48 +0100

    Add files via upload

---
+++ 26.09/26.09/26.09.cpp
@@ -0,0 +1,36 @@
+﻿#include "Header.h"
+
+void Reverse(double a[], int size)
+{
+	for (int i = 1; i < 5; i++)
+	{
+		for (int j = 0; j < 5 - i; j++)
+		{
+			if (a[j] < a[j + 1])
+			{
+				int temp = a[j];
+				a[j] = a[j + 1];
+				a[j + 1] = temp;
+			}
+		}
+	}
+
+	for (int i = 0; i < 5; i++)
+	{
+		cout << a[i];
+	}
+}
+
+int main()
+{
+	int size = 5;
+	double a[] =  {1,2,3,4,5};
+
+	for (int i = 0; i < size; i++)
+	{
+		cout << a[i];
+	}
+
+	cout << "\n";
+	Reverse(a, size);
+}
